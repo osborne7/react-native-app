d347d6a7bf37b835d1b62ab8b6389c67e0ca2c64
osborne7 <eleanor.c.osborne@gmail.com>
2020-08-04 19:53:55 -0400

    first commit

--- App.tsx
+++ App.tsx
@@ -2,20 +2,35 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+interface IHomeProps {
+
+}
+
+interface IHomeState {
+  
+}
+
+export default class App extends React.Component<IHomeProps, IHomeState> {
+  render() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textStyle}>My first React app!</Text>
+      <Text>This is neat</Text>
       <StatusBar style="auto" />
     </View>
   );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eee',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    fontSize: 20,
+    color: 'blue'
+  }
 });
